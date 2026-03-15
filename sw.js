c10d2a420684b451e51eed5147cd8706df510ed8
벨비티 <conor123481@gmail.com>
2026-03-15 13:30:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,13 @@
-const CACHE_NAME = 'macguard-v2';
-const ASSETS = [
-  './index.html',
-  './manifest.json'
-];
-
+const CACHE_NAME = 'macguard-v3';
+const ASSETS = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
 });
